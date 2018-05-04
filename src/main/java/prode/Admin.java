5746f7f4f8f9c9543358d3526f347aff02732658
nks21may <nicolasdalessandro2@gmail.com>
2018-05-04 10:38:37 -0500

    ..

--- src/main/java/prode/Admin.java
+++
@@ -1,6 +0,0 @@
-package java;
-
-//hay que volver a poner la herencia --nks
-public class Admin{
-
-}
--- src/main/java/prode/Player.java
+++
@@ -1,9 +0,0 @@
-package java;
-
-
-//Saque la herencia de user. Revista --Nks
-public class Player{
-
-	private String username;
-
-}
