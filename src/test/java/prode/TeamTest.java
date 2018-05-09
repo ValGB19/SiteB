9d701b65205637e190cbbe9ac8f5a19c185c3ba5
ValGB19 <valeriagb.unrc@gmail.com>
2018-05-09 19:02:44 -0300

    ..

---
+++ src/test/java/prode/TeamTest.java
@@ -0,0 +1,34 @@
+package prode;
+
+import prode.Team;
+
+import org.javalite.activejdbc.Base;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TeamTest {
+  @Before
+  public void before(){
+    Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode_test?nullNamePatternMatchesAll=true", "franco", "franco");
+    System.out.println("TeamTest setup");
+    Base.openTransaction();
+  }
+
+  @After
+  public void after(){
+      System.out.println("TeamTest tearDown");
+      Base.rollbackTransaction();
+      Base.close();
+  }
+
+  @Test
+  public void validatePrecenseOfTeam(){
+      Team team = new Team();
+      user.set("name", "");
+
+      assertEquals(name.isValid(), false);
+  }
+}
