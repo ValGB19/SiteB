2a14f8efbb9bf0f09bef53e3f828d9f288bb24fc
nks21may <nicolasdalessandro2@gmail.com>
2018-06-01 11:33:44 -0500

    .

--- src/main/java/prode/App.java
+++ src/main/java/prode/App.java
@@ -1,10 +1,13 @@
 package prode;
 
 import org.javalite.activejdbc.Base;
-
 import prode.User;
-
-
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import static spark.Spark.*;
+import spark.template.mustache.MustacheTemplateEngine;
+import spark.template.*;
 
 /**
  * Hello world!
@@ -13,16 +16,18 @@ import prode.User;
 public class App{
     
     public static void main( String[] args ){
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode?nullNamePatternMatchesAll=true", "franco", "franco");
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode?nullNamePatternMatchesAll=true?useSSL=false", "root", "root");
 
-        User u = new User();
-        u.set("username", "Riquelme");
-        u.set("password", "password");
-        u.saveIt();
+        Map map = new HashMap();
+	    map.put("name", "Sam");
+	    map.put("value", 1000);
+	    map.put("taxed_value", 1000 - (1000 * 0.4));
+	    map.put("in_ca", true);
 
-
-
-
+	    get("/hello", (req, res) -> {
+	        return new ModelAndView(map, "./views/hello.mustache");
+	    		}, new MustacheTemplateEngine()
+	    );
         Base.close();
     }
 }
