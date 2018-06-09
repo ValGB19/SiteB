cc2ca7687d4593c6017540556caa3bcbe3f9fd63
ValGB19 <valeriagb.unrc@gmail.com>
2018-06-08 22:31:32 -0300

    automatic

---
+++ src/migrations/20180608215345_country.sql
@@ -0,0 +1,12 @@
+insert into countries(id, name) values
+	(0,"Argentina"),
+	(0,"Chile"),
+	(0,"Uruguay"),
+	(0,"Paraguay"),
+	(0,"Peru"),
+	(0,"Colombia"),
+	(0,"Brasil"),
+	(0,"Bolivia"),
+	(0,"Venezuela"),
+	(0,"Panama"),	
+	(0,"Mexico");
