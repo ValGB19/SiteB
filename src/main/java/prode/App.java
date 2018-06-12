b2c8b2577374054361241dca2015085cc97f5a83
nicolas <nico@localhost.localdomain>
2018-06-12 21:50:17 +0000

    .

--- src/main/java/prode/App.java
+++ src/main/java/prode/App.java
@@ -18,18 +18,17 @@ public class App{
     public static void main( String[] args ){
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode?nullNamePatternMatchesAll=true", "root", "root");
 
-        Map map = new HashMap();
+       staticFiles.location("views/public");
+       Map map = new HashMap();
 	    map.put("name", "Sam");
 	    map.put("value", 1000);
 	    map.put("taxed_value", 1000 - (1000 * 0.4));
 	    map.put("in_ca", true);
 
-	    get("/inicio", (req, res) -> "./prueba/inicio.html");
-
-	    get("/hello", (req, res) -> {
-	        return new ModelAndView(map, "./prueba/inicio.html");
+	    get("/inicio", (req, res) -> {
+	        return new ModelAndView(map, "./views/inicio.mustache");
 	    		}, new MustacheTemplateEngine()
 	    );
-        Base.close();
+       Base.close();
     }
 }
