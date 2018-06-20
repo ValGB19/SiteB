d4a34361889bba94a34b2861ff02be70cb92b0ec
Nicolas Dalessandro <nicolasdalessandro2@gmail.com>
2018-06-20 12:49:02 -0300

    .

--- src/main/java/prode/App.java
+++ src/main/java/prode/App.java
@@ -2,9 +2,10 @@ package prode;
 
 import org.javalite.activejdbc.Base;
 import prode.User;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import spark.ModelAndView;
 
 import spark.*;
 import static spark.Spark.*;
@@ -43,7 +44,7 @@ public class App{
 	    post("/", (req, res) -> {
 	    	//inicio sesion
 	    	boolean log = false;
-	    	String mes="";
+	    	/*String mes="";
 	    	String usernameL = req.queryParams("usernamelogin");
 	    	String pswL = req.queryParams("pswLogin");
 	    	if(usernameL!=null && pswL!=null){
@@ -62,17 +63,21 @@ public class App{
 		    	res.status(401);
 		    	//Map<String,String> p = new HashMap();
 		    	map.put("error", mes);
-		    }
-
-	    	String nombre = req.queryParams("nombre");
-	    	String apellido = req.queryParams("apellido");
+		    }*/
 	    	String nick = req.queryParams("rUsername");
 	    	String pwd = req.queryParams("pswRegister");
-	    	String pwd2 = req.queryParams("pswValida");
+	    	String pwd2 = req.queryParams("pswValida"); 	
+	    	String nombre = req.queryParams("nombre");
+	    	String apellido = req.queryParams("apellido");
 	    	String mail = req.queryParams("mail");
 	    	String pais = req.queryParams("rpais");
 	    	String dni = req.queryParams("rdni");
-	    	if (pwd.equals(pwd2) && dni.length() == 8) {
+	    	
+	    	if (pwd2 == null && dni == null && apellido == null) {
+	    		return new ModelAndView(map, "./src/main/resources/perfil.mustache");
+	    	}
+	    	boolean e = false;
+	    	if (pwd.equals(pwd2) && dni.length() <= 8) {
 	    		User temp = new User();
 	    		temp.set("name", nombre);
 	    		temp.set("surname", apellido);
@@ -81,9 +86,27 @@ public class App{
 	    		temp.set("password", pwd);
 	    		temp.set("dni", Integer.parseInt(dni));
 	    		temp.set("country_id", Country.findFirst("name = ?", pais).get("id"));
-	    		temp.save();
+	    		e = temp.save();
 			}
-
+	    	if (!e) {
+	    		ArrayList tmp = new ArrayList();
+	    		tmp.add("Datos incorrectos");
+	    		if (pwd != pwd2) {
+	    			tmp.add("*Las contraseñas no coinciden");
+				}
+	    		if (User.findFirst("nick = ?",nick) != null) {
+	    			tmp.add("*El nickname ya esta en uso");
+				}
+	    		if (User.findFirst("dni = ?",Integer.parseInt(dni)) != null) {
+	    			tmp.add("*Ese dni ya esta registrado");
+				}
+	    		if (User.findFirst("email = ?", mail) != null) {
+	    			tmp.add("*Ese email ya esta registrado");
+				}
+				map.put("errorr", tmp);
+				System.out.println("No se registro");
+			}
+	    	
 	    	return new ModelAndView(map, "./src/main/resources/inicio.mustache");
         	}, new MustacheTemplateEngine()
 	    );
