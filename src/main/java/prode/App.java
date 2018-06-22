baeaca85561698bb241376f6c786d3a8af290685
ValGB19 <valeriagb.unrc@gmail.com>
2018-06-22 16:15:18 -0300

    automatic

--- src/main/java/prode/App.java
+++ src/main/java/prode/App.java
@@ -100,23 +100,11 @@ public class App{
 	    get("/loged/perfil", (req, res) -> {
 	    	String m = req.session().attribute("username");
 	    	User u = (User.findFirst("nick = ?",m));
-<<<<<<< HEAD
 	    	List<MatchPrediction> mpu = u.getMatchPrediction();
-	    	ArrayList<String[]> p = new ArrayList<String[]>(); 
-	    	/*/for (MatchPrediction a: mpu) {
+	    	ArrayList<Object[]> p = new ArrayList<Object[]>(); 
+	    	for (MatchPrediction a: mpu) {
 	    		p.add(a.getPartePerfil());
-	    	}*/
-	    	String[] c = new String[]{"2","0","9"};
-	    	p.add(c);
-=======
-	    	ArrayList p = new ArrayList(); 
-	    	/*List<MatchPrediction> mpu = u.getMatchPrediction();
-	    	/for (MatchPrediction a: mpu) {
-	    		p.add(a.getPartePerfil());
-	    	}*/
-	    	
-	    	p.add(new Object[]{"a","b","c"});
->>>>>>> a85e114b8752e28c2fde4b54ee3f317fe64a8ee0
+	    	}
 	    	map.put("predUser", p);
 	        return new ModelAndView(map, "./src/main/resources/loged/perfil.mustache");
 	    		}, new MustacheTemplateEngine()
