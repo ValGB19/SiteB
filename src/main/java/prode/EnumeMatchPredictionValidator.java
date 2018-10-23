670eb13c3a233021217744c2e105977c113ba28d
Nicolas Dalessandro <nicolasdalessandro2@gmail.com>
2018-10-23 09:36:50 -0300

    Upgrade code

--- src/main/java/prode/EnumeMatchPredictionValidator.java
+++ src/main/java/prode/EnumeMatchPredictionValidator.java
@@ -4,25 +4,16 @@ import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.validation.ValidatorAdapter;
 
 public class EnumeMatchPredictionValidator extends ValidatorAdapter {
-  public void validate(Model m){
-       	boolean valid = false;
-     
-		Object value = m.getString("prediction");
-
+	public void validate(Model m){
+		boolean valid = false;   
+		String value = m.getString("prediction");
         if(null == value) {
             m.addValidator(this, "prediction");
             valid = true;
-        }else{
-          if(!(value instanceof String)) {
-              throw new IllegalArgumentException("Attribute must be a String");
-          }else{
-              if("visit".equals(value) || "tie".equals(value) || "local".equals(value)){
-              valid = true;
-            }
-          }
+            return;
         }
-      System.out.println("********************************"+value);
-      if(!valid)
-         m.addValidator(this, "Bad value");
+        valid = "visit".equals(value) || "tie".equals(value) || "local".equals(value);
+      	if(!valid)
+        	m.addValidator(this, "Bad value");
     }
 }
--- src/main/java/prode/EnumeMatchValidator.java
+++ src/main/java/prode/EnumeMatchValidator.java
@@ -4,24 +4,16 @@ import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.validation.ValidatorAdapter;
 
 public class EnumeMatchValidator extends ValidatorAdapter {
-   public void validate(Model m){
-	   boolean valid = false;
-	     
-		Object value = m.getString("result");
-
-       if(null == value) {
-           m.addValidator(this, "result");
-           valid = true;
-       }else{
-         if(!(value instanceof String)) {
-             throw new IllegalArgumentException("Attribute must be a String");
-         }else{
-             if("visit".equals(value) || "tie".equals(value) || "local".equals(value) || "suspended".equals(value)){
-             valid = true;
-           }
-         }
-       }
-     if(!valid)
-        m.addValidator(this, "Bad value");
+    public void validate(Model m){
+	    boolean valid = false; 
+	    Object value = m.getString("result");
+        if(null == value){
+            m.addValidator(this, "result");
+            return;
+        }
+        if(!(value instanceof String))
+            throw new IllegalArgumentException("Attribute must be a String");
+        valid = "visit".equals(value) || "tie".equals(value) || "local".equals(value) || "suspended".equals(value);
+        if(!valid) m.addValidator(this, "Bad value");
    }
 }
