9ebcacf65a5a41c871d3099d0c7a790f13d55817
Nicolas Dalessandro <nicolasdalessandro2@gmail.com>
2018-11-15 09:21:43 -0300

    Visual changes

--- src/main/resources/admin/fn.js
+++
@@ -1,8 +0,0 @@
-function hideElem(n) {
-	var x = document.getElementById(n);
-	if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
